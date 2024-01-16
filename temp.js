b33ee3e37df4ad854b03100a0f45703f698554f4
ckk1999 <1823538605@qq.com>
2024-01-16 23:10:16 +0800

    add license

--- temp.js
+++ temp.js
@@ -7,6 +7,7 @@
 // @match        *://*
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=greasyfork.org
 // @grant        none
+// @license      MIT
 // ==/UserScript==
 
 (function() {
