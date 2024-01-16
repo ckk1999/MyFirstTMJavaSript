f76598d853e81917e5461669503cfefcb34850ec
ckk1999 <1823538605@qq.com>
2024-01-16 23:02:33 +0800

    create temp.js

---
+++ temp.js
@@ -0,0 +1,17 @@
+// ==UserScript==
+// @name         hello world!
+// @namespace    http://tampermonkey.net/
+// @version      2024-01-16
+// @description  try to take over the world!yes!
+// @author       ckk
+// @match        *://*
+// @icon         https://www.google.com/s2/favicons?sz=64&domain=greasyfork.org
+// @grant        none
+// ==/UserScript==
+
+(function() {
+    'use strict';
+
+    // Your code here...
+    console.log('hello world')
+})();
